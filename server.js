7cf7f677e8423645d1597d5a2f37bf5a28cc5f23
Bradley <bcp6x2@gmail.com>
2026-05-02 22:02:16 -0500

    Initial commit -PolicyDesk server running

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'PolicyDesk API is running!' });
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
