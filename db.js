e242eb58b9cd3d1b77a082f630063338eaac0e08
Bradley <bcp6x2@gmail.com>
2026-05-10 21:35:03 -0500

    Update db.js to support Railway PostgreSQL

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+const pool = new Pool(
+process.env.DATABASE_URL
+? {
+connectionString: process.env.DATABASE_URL,
+ssl: { rejectUnauthorized: false }
+}
+: {
 user: 'postgres',
 host: 'localhost',
 database: 'policydesk',
 password: process.env.DB_PASSWORD,
 port: 5432,
-});
+}
+);
 
 module.exports = pool;
